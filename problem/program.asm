b0f74f75653ee47ccd4471de6909e0c31f928c40
justjuliko <julioechavarria2002@gmail.com>
2022-08-26 14:25:23 -0500

    Ejemplo

--- problem/program.asm
+++ problem/program.asm
@@ -1,4 +1,58 @@
-// while (true)
 (START)			
-@START
-0;JMP
+	@KBD
+	D=M
+	@FILLORCLEAR	
+	D;JNE
+	@START
+	0;JMP
+
+(FILLORCLEAR)
+	@j
+	M = D
+	@70
+	D = D-A
+	@FILL
+	D;JEQ
+	@j
+	D = M
+	@67
+	D = D-A
+	@CLEAR
+	D;JEQ
+	@START
+	0;JMP
+
+(FILL)
+	@value
+	M = -1
+	@DRAW
+	0;JMP
+
+(CLEAR)
+	@value
+	M = 0
+	@DRAW
+	0;JMP
+
+(DRAW)
+	@SCREEN
+	D = A
+	@i
+	M = D
+
+(LOOP)
+	@value
+	D = M
+	@i
+	A = M
+	M = D
+	@i
+	M = M + 1
+	@24576
+	D = A
+	@i
+	D = M - D
+	@LOOP
+	D;JNE
+	@START
+	0;JMP
